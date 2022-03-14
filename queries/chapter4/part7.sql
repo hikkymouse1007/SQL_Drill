3d37f9614b7555d62ac4d657bbf6bb3c40ea450a
hikkymouse1007 <mc.mdt1007it@gmail.com>
2022-03-15 08:59:08 +0900

    finish chapter4-7

--- queries/chapter4/part7.sql
+++ queries/chapter4/part7.sql
@@ -1,110 +1,33 @@
 -- e.g1
-SELECT
-CustomerName AS 氏名
+DELETE
 FROM
-Customers
-UNION ALL
-SELECT
-EmployeeName AS 氏名
-FROM
-Employees;
+  Salary
+;
 
 -- practice1
-SELECT
-DepartmentID AS ID,
-DepartmentName AS 名前
+DELETE
 FROM
-Departments
-UNION ALL
-SELECT
-CategoryID AS ID,
-CategoryName AS 名前
-FROM
-Categories;
+BelongTo
+;
 
 
 -- practice2
-SELECT
-'Departments' AS テーブル名, -- 各レコードに定数を表示する
-DepartmentID AS ID,
-DepartmentName AS 名前
+DELETE
 FROM
-Departments
-UNION ALL
-SELECT
-'Categories' AS テーブル名,
-CategoryID AS ID,
-CategoryName AS 名前
-FROM
-Categories
-ORDER BY
-テーブル名,
-ID;
+Customers;
 
--- practice3, practice4
-SELECT
-'Departments' AS テーブル名, -- 各レコードに定数を表示する
-DepartmentID AS ID,
-DepartmentName AS 名前
+
+-- practice3
+DELETE
 FROM
-Departments
-UNION ALL
-SELECT
-'CustomerClasses' AS テーブル名, -- 各レコードに定数を表示する
-CustomerClassID AS ID,
-CustomerClassName AS 名前
+Sales;
+
+-- practice4
+DELETE
 FROM
-CustomerClasses
-UNION ALL
-SELECT
-'Categories' AS テーブル名,
-CategoryID AS ID,
-CategoryName AS 名前
-FROM
-Categories
-UNION ALL
-SELECT
-'Prefecturals' AS テーブル名,
-PrefecturalID AS ID,
-PrefecturalName AS 名前
-FROM
-Prefecturals
-ORDER BY
-テーブル名,
-ID;
+Products;
 
 -- practice5
-SELECT
-A.SaleID,
-A.ProductID,
-A.Quantity,
-B.CustomerClassID,
-B.CustomerName
+DELETE
 FROM
-Sales AS A
-JOIN
-Customers AS B
-ON
-A.CustomerID = B.CustomerID
-WHERE
-A.Quantity >= 10
-AND
-B.CustomerClassID = 2
-UNION ALL
-SELECT
-A.SaleID,
-A.ProductID,
-A.Quantity,
-B.CustomerClassID,
-B.CustomerName
-FROM
-Sales AS A
-JOIN
-Customers AS B
-ON
-A.CustomerID = B.CustomerID
-WHERE
-A.Quantity >= 100
-AND
-B.CustomerClassID = 1;
-
+Employees;
